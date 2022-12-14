a6a4d23d0a63d58ff1bc3fc711fd93ec6295fc96
lucasmelogithub <lucas.melo@intel.com>
2022-12-14 11:36:56 -0600

    Demo of failing policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,7 @@
 # Provision Intel Optimized AWS RDS MySQL server
 module "optimized-mysql-server" {
   source         = "intel/aws-mysql/intel"
+  instance_class = "db.m5.2xlarge"             #M5 instances cost the same as M6i but can perform 25% to 42$ less.
   db_password    = var.db_password             #Required
   rds_identifier = "intel-terraform-example02" #Required
   vpc_id         = "vpc-0590e8a9422c543ae"     #Required
