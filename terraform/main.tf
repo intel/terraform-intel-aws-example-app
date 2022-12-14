cd466b33b7fcaadb56e00d834e6cf8708c379657
lucasmelogithub <lucas.melo@intel.com>
2022-12-14 11:14:45 -0600

    Demo of failing policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ module "optimized-mysql-server" {
   db_password    = var.db_password             #Required
   rds_identifier = "intel-terraform-example02" #Required
   vpc_id         = "vpc-0590e8a9422c543ae"     #Required
-  instance_class = "db.m5.2xlarge"             #Optional
+  instance_class = "db.m5.2xlarge"             #M5 instances cost the same as M6i but can perform 25% to 42$ less. 
   db_tags = {
     Owner    = "Intel.Terraform.Example.App@intel.com"
     Duration = "8"
