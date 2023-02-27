3984e3482a165c1a98a1c64b34a1c11e22208ca5
Lucas Melo <lucas.melo@intel.com>
2023-02-27 09:35:24 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ module "optimized-mysql-server" {
   source         = "intel/aws-mysql/intel"
   db_password    = var.db_password             #Required
   rds_identifier = "intel-terraform-example02" #Required
-  vpc_id         = "vpc-0590e8a9422c543ae"     #Required
+  vpc_id         = "vpc-04cda22668fd25150"     #Required
   instance_class = "db.m5.24xlarge"            #M5 instances cost the same as M6i but can perform 25% to 42% less.
   db_tags = {                                  #Optional  
     Owner       = "Intel.Terraform.Example.App@intel.com"
