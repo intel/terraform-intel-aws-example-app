ab1602f1e581b1987ab01907f33b3c99d1f519f1
lucasmelogithub <lucas.melo@intel.com>
2023-06-07 11:36:05 -0500

    New DB

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,10 @@ module "optimized-mysql-server" {
   source         = "intel/aws-mysql/intel"
   db_password    = var.db_password
   rds_identifier = "mysql-terraform-02"
-  vpc_id = "vpc-0bf95158fd23f4e2e"
+  vpc_id         = "vpc-0bf95158fd23f4e2e"
   db_tags = {
-    "App" = "Intel Optimized Database"
-    "Owner" = "Lucas.Melo@intel.com"
+    "App"      = "Intel Optimized Database"
+    "Owner"    = "Lucas.Melo@intel.com"
     "Duration" = "8"
   }
 }
